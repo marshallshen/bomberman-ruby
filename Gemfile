a26d9262c327f3aee7210e86f83e1b7633b71d24
LupineDev <lupinedev@gmail.com>
2013-01-23 15:46:10 -1000

    Created gem skeleton via bundler

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in narya-client.gemspec
+gemspec
---
+++ lib/narya-client.rb
@@ -0,0 +1,8 @@
+require 'narya-client/version'
+require 'rest-client'
+
+module Narya
+  module Client
+    # Your code goes here...
+  end
+end
