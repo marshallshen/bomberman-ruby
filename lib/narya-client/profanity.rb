6e57fc5f68934537aafa180ebed77a0b2c61a380
LupineDev <lupinedev@gmail.com>
2013-02-28 15:57:00 -1000

    Added support for hilight and censor requests to api

--- lib/narya-client/profanity.rb
+++ lib/narya-client/profanity.rb
@@ -1,3 +1,4 @@
+require 'json'
 module Narya
   module Client
     module Profanity
@@ -6,12 +7,46 @@ module Narya
           req.url "v#{Narya::Client.api_version}/profanity/check", corpus: corpus
         end
 
+        validate_status(request)
+
         if request.status == 200
           request.body.eql?("1") ? true : false
-        elsif request.status == 400
+        end
+      end
+
+      def self.censor(corpus=nil, replacement_text=nil)
+        request = Narya::Client.connection.get do |req|
+          req.url "v#{Narya::Client.api_version}/profanity/censor", corpus: corpus, replacement_text: replacement_text
+        end
+
+        validate_status(request)
+
+        if request.status == 200
+          JSON.parse(request.body)['censored_text']
+        end
+      end
+
+      def self.highlight(corpus=nil, start_tag=nil, end_tag=nil)
+        request = Narya::Client.connection.get do |req|
+          req.url "v#{Narya::Client.api_version}/profanity/highlight", corpus: corpus, start_tag: start_tag, end_tag: end_tag
+        end
+
+        validate_status(request)
+
+        if request.status == 200
+          JSON.parse(request.body)['highlighted_text']
+        end
+      end
+
+      private
+
+      def self.validate_status(request)
+        if request.status == 400
           raise Narya::Client::BadRequest
         elsif request.status == 401
           raise Narya::Client::Unauthorized
+        elsif request.status == 500
+          raise Narya::Client::InternalServerError
         end
       end
     end
--- narya-client.gemspec
+++ narya-client.gemspec
@@ -17,5 +17,6 @@ Gem::Specification.new do |gem|
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.require_paths = ["lib"]
   gem.add_runtime_dependency "faraday", ["0.8.5"]
+  gem.add_runtime_dependency "json", ["1.7.7"]
   gem.add_development_dependency "rspec", ["2.12.0"]
 end
