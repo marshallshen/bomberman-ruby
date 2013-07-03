13bd941d23500682a8a6c615bb2c4f0d776d347c
Marshall Shen <mshen@groupon.com>
2013-07-03 12:07:41 -0500

    refactored connection, improve specs

--- lib/bomberman/profanity.rb
+++ lib/bomberman/profanity.rb
@@ -2,63 +2,18 @@ require 'json'
 module Bomberman
   module Profanity
     def self.profane?(corpus=nil, language=:en)
-      request = Bomberman.connection.get do |req|
-        req.url "#{lang_api_version(language)}/profanity/check", corpus: corpus
-      end
-
-      validate_status(request)
-
-      if request.status == 200
-        request.body.eql?("1") ? true : false
-      end
+      response = Bomberman.connection.response_for("#{Bomberman.connection.lang_api_version(language)}/profanity/check", corpus: corpus)
+      response.body.eql?("1") ? true : false
     end
 
     def self.censor(corpus=nil, replacement_text=nil, language=:en)
-      request = Bomberman.connection.get do |req|
-        req.url "#{lang_api_version(language)}/profanity/censor", corpus: corpus, replacement_text: replacement_text
-      end
-
-      validate_status(request)
-
-      if request.status == 200
-        JSON.parse(request.body)['censored_text']
-      end
+      response = Bomberman.connection.response_for("#{Bomberman.connection.lang_api_version(language)}/profanity/censor", corpus: corpus, replacement_text: replacement_text)
+      JSON.parse(response.body)['censored_text']
     end
 
     def self.highlight(corpus=nil, start_tag=nil, end_tag=nil, language=:en)
-      request = Bomberman.connection.get do |req|
-        req.url "#{lang_api_version(language)}/profanity/highlight", corpus: corpus, start_tag: start_tag, end_tag: end_tag
-      end
-
-      validate_status(request)
-
-      if request.status == 200
-        JSON.parse(request.body)['highlighted_text']
-      end
-    end
-
-    private
-
-    def self.lang_api_version(language=:en)
-      if language == :en
-       "api/v#{Bomberman.api_version}"
-      elsif language == :ja
-       "api/#{language}/v#{Bomberman.api_version}"
-      else
-        raise Bomberman::LanguageNotSupported
-      end
-    end
-
-    def self.validate_status(request)
-      if request.status == 400
-        raise Bomberman::BadRequest
-      elsif request.status == 401
-        raise Bomberman::Unauthorized
-      elsif request.status == 403
-        raise Bomberman::RateLimitExceeded
-      elsif request.status == 500
-        raise Bomberman::InternalServerError
-      end
+      response = Bomberman.connection.response_for("#{Bomberman.connection.lang_api_version(language)}/profanity/highlight", corpus: corpus, start_tag: start_tag, end_tag: end_tag)
+      JSON.parse(response.body)['highlighted_text']
     end
   end
 end
--- spec/lib/bomberman/connection_spec.rb
+++ spec/lib/bomberman/connection_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 describe Bomberman::Connection do
   before :all do
     Bomberman.configure {}
+    @connection = Bomberman::Connection.new
   end
 
   it 'should inherit from Faraday::Connection' do
@@ -11,10 +12,6 @@ describe Bomberman::Connection do
   end
 
   describe "constructor" do
-    before :all do
-      @connection = Bomberman::Connection.new
-    end
-
     it "should initialize with url based on config values" do
       @connection.host.should eq(Bomberman.url)
     end
@@ -26,6 +23,29 @@ describe Bomberman::Connection do
     it "should set the headers correct api authorization params" do
       auth_headers = "Token token=#{Bomberman.api_key}"
       @connection.headers['Authorization'].should eq(auth_headers)
+    end
+  end
+
+  describe "response_for" do
+    it "returns response if status code is 200" do
+      mock_response = mock("response", status: 200, body: "response")
+      @connection.stub(:get).and_return(mock_response)
+      @connection.response_for("some_url", foo: :bar).should == mock_response
+    end
+    it "raise Bomberman-based error if status code is a known error code" do
+      mock_error = mock("response", status: 400, error: "error_text")
+      @connection.stub(:get).and_return(mock_error)
+      expect{
+        @connection.response_for("some_url", foo: :bar)
+      }.to raise_error(Bomberman::BadRequest)
+    end
+  end
+
+  describe "language_api_version" do
+    it "returns url based language" do
+      @connection.lang_api_version(:en).should == "api/v1"
+      @connection.lang_api_version(:ja).should == "api/ja/v1"
+      expect { @connection.lang_api_version(:ch) }.to raise_error(Bomberman::LanguageNotSupported)
     end
   end
 end
