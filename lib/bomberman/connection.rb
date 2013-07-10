d0ff79adc46c7052c0ca30f4b841ff9ba96c0d2b
Marshall Shen <mshen@groupon.com>
2013-07-10 12:19:18 -0500

    Revert "refactored connection, improve specs"
    This reverts commit 13bd941d23500682a8a6c615bb2c4f0d776d347c.

--- lib/bomberman/connection.rb
+++ lib/bomberman/connection.rb
@@ -8,34 +8,5 @@ module Bomberman
       self.headers['Authorization'] = "Token token=#{Bomberman.api_key}"
       self
     end
-
-    def response_for(url, options = {})
-      response = get do |request|
-        request.url url, options
-      end
-
-      return response if response.status == 200
-      transform_error_status(response)
-    end
-
-    def lang_api_version(language=:en)
-      if language == :en
-       "api/v#{Bomberman.api_version}"
-      elsif language == :ja
-       "api/#{language}/v#{Bomberman.api_version}"
-      else
-        raise Bomberman::LanguageNotSupported
-      end
-    end
-
-    private
-    def transform_error_status(response)
-      case response.status
-      when 400 then raise Bomberman::BadRequest
-      when 401 then raise Bomberman::Unauthorized
-      when 403 then raise Bomberman::RateLimitExceeded
-      when 500 then raise Bomberman::InternalServerError
-      end
-    end
   end
 end
--- lib/bomberman/profanity.rb
+++ lib/bomberman/profanity.rb
@@ -2,18 +2,63 @@ require 'json'
 module Bomberman
   module Profanity
     def self.profane?(corpus=nil, language=:en)
-      response = Bomberman.connection.response_for("#{Bomberman.connection.lang_api_version(language)}/profanity/check", corpus: corpus)
-      response.body.eql?("1") ? true : false
+      request = Bomberman.connection.get do |req|
+        req.url "#{lang_api_version(language)}/profanity/check", corpus: corpus
+      end
+
+      validate_status(request)
+
+      if request.status == 200
+        request.body.eql?("1") ? true : false
+      end
     end
 
     def self.censor(corpus=nil, replacement_text=nil, language=:en)
-      response = Bomberman.connection.response_for("#{Bomberman.connection.lang_api_version(language)}/profanity/censor", corpus: corpus, replacement_text: replacement_text)
-      JSON.parse(response.body)['censored_text']
+      request = Bomberman.connection.get do |req|
+        req.url "#{lang_api_version(language)}/profanity/censor", corpus: corpus, replacement_text: replacement_text
+      end
+
+      validate_status(request)
+
+      if request.status == 200
+        JSON.parse(request.body)['censored_text']
+      end
     end
 
     def self.highlight(corpus=nil, start_tag=nil, end_tag=nil, language=:en)
-      response = Bomberman.connection.response_for("#{Bomberman.connection.lang_api_version(language)}/profanity/highlight", corpus: corpus, start_tag: start_tag, end_tag: end_tag)
-      JSON.parse(response.body)['highlighted_text']
+      request = Bomberman.connection.get do |req|
+        req.url "#{lang_api_version(language)}/profanity/highlight", corpus: corpus, start_tag: start_tag, end_tag: end_tag
+      end
+
+      validate_status(request)
+
+      if request.status == 200
+        JSON.parse(request.body)['highlighted_text']
+      end
+    end
+
+    private
+
+    def self.lang_api_version(language=:en)
+      if language == :en
+       "api/v#{Bomberman.api_version}"
+      elsif language == :ja
+       "api/#{language}/v#{Bomberman.api_version}"
+      else
+        raise Bomberman::LanguageNotSupported
+      end
+    end
+
+    def self.validate_status(request)
+      if request.status == 400
+        raise Bomberman::BadRequest
+      elsif request.status == 401
+        raise Bomberman::Unauthorized
+      elsif request.status == 403
+        raise Bomberman::RateLimitExceeded
+      elsif request.status == 500
+        raise Bomberman::InternalServerError
+      end
     end
   end
 end
